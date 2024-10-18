21bb134f31c4c950509420e1b49c3c89bb8a668a
Flo <98991486+Florian-Lup@users.noreply.github.com>
2024-10-18 10:23:46 +0100

    Update MenuItem.tsx

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,17 +1,33 @@
-import './MenuItem.scss'
+// MenuItem.jsx
+import React from 'react';
+import './MenuItem.scss';
+import remixiconUrl from 'remixicon/fonts/remixicon.symbol.svg';
 
-import remixiconUrl from 'remixicon/fonts/remixicon.symbol.svg'
+interface MenuItemProps {
+  icon?: string;
+  title?: string;
+  action?: () => void;
+  isActive?: boolean;
+}
 
-export default function MenuItem ({
-  icon, title, action, isActive = null,
-}: {icon?: string, title?: string, action?: () => void, isActive?: (() => boolean) | null } ) {
-  return <button
-      className={`menu-item${isActive && isActive() ? ' is-active' : ''}`}
+const MenuItem: React.FC<MenuItemProps> = ({
+  icon,
+  title,
+  action,
+  isActive = false,
+}) => {
+  return (
+    <button
+      className={`menu-item${isActive ? ' is-active' : ''}`}
       onClick={action}
       title={title}
+      type="button" // Ensures the button doesn't submit forms if inside one
     >
       <svg className="remix">
-        <use xlinkHref={`${remixiconUrl}#ri-${icon}`}/>
+        <use xlinkHref={`${remixiconUrl}#ri-${icon}`} />
       </svg>
     </button>
-}
+  );
+};
+
+export default React.memo(MenuItem); // Optimize with React.memo
