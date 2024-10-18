0e3e20fe5663a43927dc239edbd2f4a612e79f6f
Flo <98991486+Florian-Lup@users.noreply.github.com>
2024-10-18 11:27:18 +0100

    Update MenuItem.tsx

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,21 +1,17 @@
-import './MenuItem.scss';
-import remixiconUrl from 'remixicon/fonts/remixicon.symbol.svg';
+import './MenuItem.scss'
 
-export default function MenuItem({
-  icon,
-  title,
-  action,
-  isActive = null,
-}) {
-  return (
-    <button
+import remixiconUrl from 'remixicon/fonts/remixicon.symbol.svg'
+
+export default function MenuItem ({
+  icon, title, action, isActive = null,
+}: {icon?: string, title?: string, action?: () => void, isActive?: (() => boolean) | null } ) {
+  return <button
       className={`menu-item${isActive && isActive() ? ' is-active' : ''}`}
       onClick={action}
       title={title}
     >
       <svg className="remix">
-        <use xlinkHref={`${remixiconUrl}#ri-${icon}`} />
+        <use xlinkHref={`${remixiconUrl}#ri-${icon}`}/>
       </svg>
     </button>
-  );
 }
