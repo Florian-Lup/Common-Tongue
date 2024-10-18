6424d9054a31d1925b54c1672217095312f12427
Flo <98991486+Florian-Lup@users.noreply.github.com>
2024-10-18 13:58:36 +0100

    Update MenuItem.tsx

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,17 +1,32 @@
-import './MenuItem.scss'
+// MenuItem.tsx
+import './MenuItem.scss';
+import remixiconUrl from 'remixicon/fonts/remixicon.symbol.svg';
 
-import remixiconUrl from 'remixicon/fonts/remixicon.symbol.svg'
-
-export default function MenuItem ({
-  icon, title, action, isActive = null,
-}: {icon?: string, title?: string, action?: () => void, isActive?: (() => boolean) | null } ) {
-  return <button
+export default function MenuItem({
+  icon,
+  title,
+  action,
+  isActive = null,
+  onMenuItemClick,
+}: {
+  icon?: string;
+  title?: string;
+  action?: () => void;
+  isActive?: (() => boolean) | null;
+  onMenuItemClick?: () => void;
+}) {
+  return (
+    <button
       className={`menu-item${isActive && isActive() ? ' is-active' : ''}`}
-      onClick={action}
+      onClick={() => {
+        action();
+        if (onMenuItemClick) onMenuItemClick();
+      }}
       title={title}
     >
       <svg className="remix">
-        <use xlinkHref={`${remixiconUrl}#ri-${icon}`}/>
+        <use xlinkHref={`${remixiconUrl}#ri-${icon}`} />
       </svg>
     </button>
+  );
 }
