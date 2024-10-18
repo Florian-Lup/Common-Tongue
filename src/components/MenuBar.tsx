9289e42d31da56d54e753f5347dfb4e9776ccfcc
Flo <98991486+Florian-Lup@users.noreply.github.com>
2024-10-18 11:39:14 +0100

    Update MenuBar.tsx

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -255,7 +255,6 @@ export default function MenuBar({ editor }: { editor: Editor }) {
 
   return (
     <div className="editor__header">
-      <div className="menu-bar">
         {items.map((item, index) => (
           <Fragment key={index}>
             {item.type === "divider" ? (
@@ -265,7 +264,6 @@ export default function MenuBar({ editor }: { editor: Editor }) {
             )}
           </Fragment>
         ))}
-      </div>
     </div>
   );
 }
