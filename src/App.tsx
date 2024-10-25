d7e6cd5a2849e247a079a2e6d27efbd6af8cb4da
Flo <98991486+Florian-Lup@users.noreply.github.com>
2024-10-25 11:39:39 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const App: React.FC = () => {
         mode: 'shallowest',
       }),
     ],
-    editable: !isTyping && !isProcessing,
+    // Removed 'editable' prop
     onUpdate: ({ editor }) => {
       setCharacterCount(editor.storage.characterCount.characters());
     },
