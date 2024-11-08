599fb016c62dffbdb6e088559f75d37ba5957252
Flo <98991486+Florian-Lup@users.noreply.github.com>
2024-11-08 21:03:20 +0000

    Update fixGrammar.ts

--- api/fixGrammar.ts
+++ api/fixGrammar.ts
@@ -25,7 +25,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     const requestBody = {
       inputs: { manuscript },
-      version: '^1.4', // Ensure the correct version is used
+      version: '^1.5', // Ensure the correct version is used
     };
 
     const response = await fetch(apiUrl, {
